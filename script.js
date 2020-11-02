dafd7fdb59b622de2b5d3cb3c6ffb726dc6f5d40
Julia Shlykova <julia.shlykova17@gmail.com>
2020-11-02 14:17:19 +0300

    Add script.js

---
+++ script.js
@@ -0,0 +1,106 @@
+//Library
+let myLibrary = [];
+
+function Book(title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+Book.prototype.info = function(){
+    return `"${this.title}" by ${this.author}, ${this.pages} pages, ${this.read}`;
+};
+//creating some examples
+const myBook = new Book('Frankenstein', 'M. W. Shelley', 166, 'not read yet');
+myLibrary.push(myBook);
+const myBook2 = new Book('Dracula', 'B. Stoker', 326, 'not read yet');
+myLibrary.push(myBook2);
+const myBook3 = new Book('The Woman in Black', 'S. Hill', 208, 'not read yet');
+myLibrary.push(myBook3);
+const myBook4 = new Book('It', 'S. King', 1184, 'not read yet');
+myLibrary.push(myBook4);
+let shelves = document.querySelector('#shelves');
+function toggleRead(){
+    let codeInLibrary = this.parentNode.getAttribute('code-in-library');
+    let book = myLibrary[codeInLibrary];
+    if(book.read === 'not read yet'){
+        book.read = 'already read';
+    }
+    else{
+        book.read = 'not read yet';
+    }
+    let p = this.previousSibling;
+    p.textContent = `Title: ${book.title}\nAuthor: ${book.author}\n${book.pages} pages\n${book.read}`;
+    console.log(this.parentNode);
+}
+function removeBook(){
+    let codeInLibrary = this.parentNode.getAttribute('code-in-library');
+    myLibrary.splice(codeInLibrary,1);
+    shelves.textContent='';
+    for (let book of myLibrary){
+        putBookOnShelf(book);
+    }
+}
+function putBookOnShelf(book){
+    let el = document.createElement("div");
+    el.setAttribute('code-in-library',myLibrary.indexOf(book));
+    let p = document.createElement("p");
+    p.setAttribute('style','white-space:pre');
+    p.textContent = `Title: ${book.title}\nAuthor: ${book.author}\n${book.pages} pages\n${book.read}`;
+    el.appendChild(p);
+    let btnToggleRead = document.createElement("button");
+    btnToggleRead.classList.add('btn-toggle-read');
+    btnToggleRead.textContent = 'Change read status';
+    btnToggleRead.addEventListener('click',toggleRead);
+    el.appendChild(btnToggleRead);
+    let btnRemoveBook = document.createElement("button");
+    btnRemoveBook.classList.add('btn-remove-book');
+    btnRemoveBook.textContent = "Remove\n book";
+    btnRemoveBook.addEventListener('click',removeBook);
+    el.appendChild(btnRemoveBook);
+    let shelf = document.createElement("div");
+    shelf.classList.add('shelf');
+    el.appendChild(shelf);
+    shelves.appendChild(el);
+}
+for (let book of myLibrary){
+    putBookOnShelf(book);
+}
+
+
+function addBookToLibrary(){
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#number-of-pages').value;
+    let read = '';
+    let radioBtns = document.querySelectorAll('input[name="read-or-not"');
+    radioBtns.forEach(radio=>{
+        if(radio.checked){
+            if (radio.id=="no"){
+                read = 'not read yet';
+            }
+            else{
+                read = 'already read';
+            }
+        }
+    })
+    if (title===''||author===''||pages===''||read===''){
+        alert("Please, fill every field")
+    }
+    else{
+        let newBook = new Book(title,author,pages,read);
+        myLibrary.push(newBook);
+        putBookOnShelf(newBook);
+        form.removeAttribute('style');
+        btnOpenForm.removeAttribute('style');
+    }
+
+}
+const btnOpenForm = document.querySelector("#open-form");
+const form = document.querySelector('#container-for-form');
+btnOpenForm.addEventListener('click',function(){
+    form.setAttribute('style','display: block');
+    btnOpenForm.setAttribute('style','display:none');
+})
+const btnAddBook = document.querySelector("#btn-add");
+btnAddBook.addEventListener('click',addBookToLibrary);
