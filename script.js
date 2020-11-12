5d2a76d50948930c2f8904ab49c14948d703409c
Julia Shlykova <julia.shlykova17@gmail.com>
2020-11-12 14:50:21 +0300

    Creating some OOP features

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-//Library
-let myLibrary = [];
-
-function Book(title,author,pages,read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+//Rewritting JS using ES6 classes
+class Book{
+    constructor(title,author,pages,read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    info(){
+        return `"${this.title}" by ${this.author}, ${this.pages} pages, ${this.read}`;
+    }
 }
-Book.prototype.info = function(){
-    return `"${this.title}" by ${this.author}, ${this.pages} pages, ${this.read}`;
-};
-//creating some examples
+class Library{
+    constructor(myLibrary=[]){
+        this.books = myLibrary;
+    }
+}
+const myLibrary = new Library();
+
 const myBook = new Book('Frankenstein', 'M. W. Shelley', 166, 'not read yet');
-myLibrary.push(myBook);
+myLibrary.books.push(myBook);
 const myBook2 = new Book('Dracula', 'B. Stoker', 326, 'not read yet');
-myLibrary.push(myBook2);
+myLibrary.books.push(myBook2);
 const myBook3 = new Book('The Woman in Black', 'S. Hill', 208, 'not read yet');
-myLibrary.push(myBook3);
+myLibrary.books.push(myBook3);
 const myBook4 = new Book('It', 'S. King', 1184, 'not read yet');
-myLibrary.push(myBook4);
+myLibrary.books.push(myBook4);
 let shelves = document.querySelector('#shelves');
 function toggleRead(){
     let codeInLibrary = this.parentNode.getAttribute('code-in-library');
-    let book = myLibrary[codeInLibrary];
+    let book = myLibrary.books[codeInLibrary];
     if(book.read === 'not read yet'){
         book.read = 'already read';
     }
@@ -35,15 +41,15 @@ function toggleRead(){
 }
 function removeBook(){
     let codeInLibrary = this.parentNode.getAttribute('code-in-library');
-    myLibrary.splice(codeInLibrary,1);
+    myLibrary.books.splice(codeInLibrary,1);
     shelves.textContent='';
-    for (let book of myLibrary){
+    for (let book of myLibrary.books){
         putBookOnShelf(book);
     }
 }
 function putBookOnShelf(book){
     let el = document.createElement("div");
-    el.setAttribute('code-in-library',myLibrary.indexOf(book));
+    el.setAttribute('code-in-library',myLibrary.books.indexOf(book));
     let p = document.createElement("p");
     p.setAttribute('style','white-space:pre');
     p.textContent = `Title: ${book.title}\nAuthor: ${book.author}\n${book.pages} pages\n${book.read}`;
@@ -63,7 +69,7 @@ function putBookOnShelf(book){
     el.appendChild(shelf);
     shelves.appendChild(el);
 }
-for (let book of myLibrary){
+for (let book of myLibrary.books){
     putBookOnShelf(book);
 }
 
@@ -89,7 +95,7 @@ function addBookToLibrary(){
     }
     else{
         let newBook = new Book(title,author,pages,read);
-        myLibrary.push(newBook);
+        myLibrary.books.push(newBook);
         putBookOnShelf(newBook);
         form.removeAttribute('style');
         btnOpenForm.removeAttribute('style');
